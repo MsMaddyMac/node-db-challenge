49c18388903c6e8973f634154d668936b28d1c9b
Maddy <mmcintosh2k14@gmail.com>
2019-12-13 13:18:15 -0500

    creates server.js functionality

--- api/server.js
+++ api/server.js
@@ -4,3 +4,22 @@ const helmet = require('helmet');
 const projectsRouter = require('../businessProjects/routers/projects-router');
 
 const server = express();
+
+server.get('/', (req, res) => {
+    res.send("<h2>Sprint time!!<h2>");
+});
+
+//custom middleware
+function logger(req, res, next) {
+    console.log(`[${new Date().toISOString()}] ${req.method} to ${req.originalUrl}`)
+
+    next();
+};
+
+server.use(helmet());
+server.use(express.json());
+server.use(logger);
+
+server.use('/api/projects', projectsRouter);
+
+module.exports = server;
