9c9f931ae1557c1070e5de9b0a78e186fa8d7e45
Maddy <mmcintosh2k14@gmail.com>
2019-12-13 14:08:10 -0500

    creates endpoint to add project

--- businessProjects/routers/projects-router.js
+++ businessProjects/routers/projects-router.js
@@ -4,4 +4,33 @@ const Projects = require('../models/projects-model');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    Projects.getProjects(req.query)
+    .then(projects => {
+        res
+        .status(200)
+        .json(projects);
+    })
+    .catch(err => {
+        console.log('Could not get projects.', err);
+        res
+        .status(500)
+        .json({ error: 'Could not get projects.' })
+    });
+});
+
+router.post('/', (req, res) => {
+    Projects.add(req.body)
+    .then(project => {
+        res.status(201).json(project);
+    })
+    .catch(err => {
+        console.log('Could not add project.');
+        res.status(500).json({
+
+            error: 'Could not add project.'
+        });
+    });
+});
+
 module.exports = router;
